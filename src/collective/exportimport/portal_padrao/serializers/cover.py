f627de401d7fc58fe72a16e2a519ccfb4d8424ba
Érico Andrei <ericof@gmail.com>
2025-12-09 15:29:37 -0300

    Fix conditional import

--- src/collective/exportimport/portal_padrao/serializers/cover.py
+++ src/collective/exportimport/portal_padrao/serializers/cover.py
@@ -1,11 +1,14 @@
+import base64
+
+from plone.namedfile.file import NamedBlobImage
 from plone.restapi.interfaces import ISerializeToJson
 from plone.restapi.serializer.converters import json_compatible
 from plone.restapi.serializer.dxcontent import SerializeFolderToJson
-from zope.component import adapter, getMultiAdapter, queryMultiAdapter
+from zope.component import adapter, queryMultiAdapter
 from zope.interface import Interface, implementer
 
 from collective.cover.interfaces import ICover
-from collective.nitf.interfaces import INITF
+from collective.cover.tiles.base import IPersistentCoverTile
 
 
 @implementer(ISerializeToJson)
@@ -48,4 +51,34 @@ class CoverJSONSerializer(SerializeFolderToJson):
                 }
             )
         )
+        return result
+
+
+@implementer(ISerializeToJson)
+@adapter(IPersistentCoverTile, Interface)
+class TileJSONSerializer:
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        obj = self.context
+        data = obj.data
+        tile_name = obj.__name__
+        uuid = obj.data.get("uuid", "")
+        result = {
+            "UID": uuid,
+            "type": tile_name,
+        }
+        for name, value in data.items():
+            if isinstance(value, NamedBlobImage):
+                value = {
+                    "filename": value.filename,
+                    "content-type": value.contentType,
+                    "data": base64.b64encode(value.data),
+                    "encoding": "base64",
+                }
+            else:
+                value = json_compatible(value)
+            result[json_compatible(name)] = value
         return result
